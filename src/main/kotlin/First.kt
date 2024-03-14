6a38a3dfd38ba41ae3389a4e9b4095472629f806
Наталья Николаенко <natalanikolaenko@MacBook-Air-Natala.local>
2024-03-14 20:59:40 +0300

    Initial commit

---
+++ src/main/kotlin/First.kt
@@ -0,0 +1,52 @@
+
+fun main(args: Array<String>) {
+
+    // Практическая №1
+    fun printDifferentValues(){
+        println("Иван")
+        println(35)
+        println(true)
+    }
+
+    fun variableSetting(){
+        var intValue: Int = 5
+        var charValue: Char = 'F'
+        var stringValue: String = "Привет"
+        var doubleValue: Double = 90.2
+
+        val immutableValue: Int = 67
+
+        println(stringValue)
+    }
+
+    fun numberDivider(fullNumber:Int): List<String>{
+        return fullNumber.toString().trim().split("").toList()
+    }
+    //println(numberDivider(123).forEach { e-> println(e) })
+
+    fun daysToFullDataConverter(weeksCounter:Int):String{
+
+        println(weeksCounter/365)
+        println(weeksCounter%365/7)
+        println(weeksCounter%365%7)
+        return "Лет: " + weeksCounter/365 + ",недель: " + weeksCounter%365/7 + ",дней: 3"
+    }
+    //println(daysToFullDataConverter(2642))
+
+
+    fun numberManipulations(number:Int):Int{
+        return (number.toString() + (number*2).toString()).toInt()
+    }
+    //println(numberManipulations(23))
+
+
+    fun helloConsoleInterfaceApp(){
+        println("Здравствуйте, введите ваше имя.")
+        val name = readLine()
+        println("Теперь введите ваш возраст.")
+        val age = readLine()
+        println("Привет, $name! Тебе уже $age лет!")
+    }
+
+    //helloConsoleInterfaceApp()
+}
---
+++ src/main/kotlin/Second.kt
@@ -0,0 +1,88 @@
+import java.lang.Integer.min
+import javax.management.InvalidAttributeValueException
+import kotlin.math.abs
+import kotlin.text.toInt as toInt
+
+fun main(args: Array<String>) {
+
+    fun divisionReminder(){
+        if(20%17 > 0) println("Получилось значение большее чем ноль!")
+    }
+
+
+    fun ageChecker(){
+        val age:Int = readLine()?.toInt() ?: 0;
+        if(age>18) println("Вам уже все можно")
+        if(age==18) println("Ура, Вам 18 лет!")
+        if(age<18) println("Вы еще слишком молоды")
+    }
+
+
+    fun simpleCalculator(){
+        val x = 2
+        val y = 6
+        println("Выберете действие, введите число, которое соответсвует дейсвию:\n1.Добавить\n2.Отнять\n0.Ничего")
+        val actionNumber = readLine()!!.toInt()
+        when(actionNumber){
+            1 -> println(x+y)
+            2 -> println(x-y)
+        }
+    }
+
+    fun intToWeekDayConverter(day:String){
+        when(day.toInt()){
+            1 -> println("Понедельник")
+            2 -> println("Вторник")
+            3 -> println("Среда")
+            4 -> println("Четверг")
+            5 -> println("Пятница")
+            6 -> println("Суббота")
+            7 -> println("Воскресенье")
+        }
+    }
+
+
+    fun findingTheAverageNumber(x:Int,y:Int,z:Int): Int{
+        if(x==y || x==z || y==z){
+            throw InvalidAttributeValueException("Ошибка")
+        }else{
+            return when{
+                (x in z until y) || (x in y  until z) -> x
+                (y in z until x) || (y in x  until z) -> y
+                else -> z
+            }
+        }
+    }
+
+
+//    println(findingTheAverageNumber(1, 2, 3))
+
+
+
+    fun calculatingProfits(expenses:Int,income:Int){
+        val result = (income-expenses);
+         when{
+            (result >= 0) -> println("Ваша прибыль составила $result")
+             else -> println("Ваши убытки составили ${abs(result)}")
+        }
+    }
+
+//    calculatingProfits(750,345)
+
+
+    fun variableChecking(x:Int,y:Int): Boolean{
+        return when{
+            x%2==0 && y%2==0 -> true
+            else -> false
+        }
+//        return x%2==0 && y%2==0;
+    }
+
+    println(variableChecking(2, 0))
+
+
+}
+
+
+
+
